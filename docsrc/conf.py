cc18ad6cc48268a0ede5728eaada07bb8296da41
UnigisDocs <josep.villalobos@unigis.com>
2024-02-27 14:10:35 -0600

    initial commit

---
+++ docsrc/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Release Notes'
+copyright = '2024, UNIGIS TMS'
+author = 'UNIGIS TMS'
+release = '10.200.21.0.-LTS'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'furo'
+html_static_path = ['_static']
---
+++ docsrc/index.rst
@@ -0,0 +1,22 @@
+.. Release Notes documentation master file, created by
+   sphinx-quickstart on Tue Feb 27 14:07:58 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Release Notes
+=============
+
+Descripción de prueba para visualizar 
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
